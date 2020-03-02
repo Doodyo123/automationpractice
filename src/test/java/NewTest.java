a9352fd662a0dafdf611f70d8b77b9182813c707
Derick Ho <Derickho94@yahoo.com>
2020-03-02 12:07:40 -0800

    Added webelement and webdriver tests.

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,18 +1,54 @@
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class NewTest {
-  @Test
-  public void f() {
-	  System.out.println("Hello World!");
-  }
-  @BeforeTest
-  public void beforeTest() {
-  }
 
-  @AfterTest
-  public void afterTest() {
-  }
+	private WebDriver driver;
+
+	@Test
+	public void f() {
+		System.out.println("Hello World!");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		String url = "http://google.com";
+
+		driver.get(url);
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+		
+		driver.quit();
+		
+	}
+
+	@Test
+	public void launchChromeDriver() {
+
+		Assert.assertEquals(driver.getTitle(), "Google", "An incorrect page was loaded.");
+		
+	}
+	
+	@Test
+	public void canClickLuckyButton() {
+		
+		WebElement luckyButton  =  driver.findElement(By.id("gbqfbb"));
+		luckyButton.click();
+				
+	}
 
 }
