85fd381dddaae1fa8acae6a4b6598810277dc3d7
Derick Ho <Derickho94@yahoo.com>
2020-03-03 18:34:15 -0800

    added page factory

--- src/test/java/DropdownListTest.java
+++ src/test/java/DropdownListTest.java
@@ -1,48 +1,31 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class DropdownListTest {
-
-	private WebDriver driver;
-
-	@BeforeTest
-	public void beforeTest() {
-		driver = getWebDriver();
-	}
-
-	private WebDriver getWebDriver() {
-		System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
-		driver = new ChromeDriver();
-		String url = "http://the-internet.herokuapp.com/dropdown";
-		driver.get(url);
-		return driver;
-	}
+public class DropdownListTest extends TestSuperClass{
 
 	// As a user
 	// I want to select option 2 from a dropdown list
 	// So that some option is selected
 	@Test
 	public void canSelectFromDrowdown() {
-		
-		WebElement dropDown = driver.findElement(By.id("dropdown"));
-		WebElement secondOption = driver.findElement(By.linkText("Option 2"));
-		
-		dropDown.click();
-		secondOption.click();
-		
-		String actualElement = "Option 2";
-		Assert.assertEquals(secondOption.getText(), actualElement, "Failed to click Option 2!");
-	}
+		//TODO navigate to the dropdown page.
 
-	@AfterTest
-	public void afterTest() {
-		driver.close();
+		String expectedSelection = "Option 2";
+
+		//WebElement dropDown = driver.findElement(By.id("dropdown"));
+		//WebElement secondOption = driver.findElement(By.linkText("Option 2"));
+
+		Select customerDropDownList = new Select(driver.findElement(By.id("dropdown")));
+		customerDropDownList.selectByIndex(2);
+
+		//dropDown.click();
+		//secondOption.click();
+
+		String actualSelection = customerDropDownList.getFirstSelectedOption().getText();
+
+		Assert.assertEquals(actualSelection, expectedSelection, "Failed to click Option 2!");
 	}
 
 }
